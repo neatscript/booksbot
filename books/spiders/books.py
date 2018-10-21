a3eeeb3e9d4a531924cec400fe3aeb758892e191
neatscript <ashish.ranjanacb6109@gmail.com>
2018-10-21 18:42:12 +0530

    Update books.py

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -4,9 +4,9 @@ import scrapy
 
 class BooksSpider(scrapy.Spider):
     name = "books"
-    allowed_domains = ["books.toscrape.com"]
+    
     start_urls = [
-        'http://books.toscrape.com/',
+        'http://aparnachoudhuri.blogspot.com/2016/03/here-i-write-my-post-as-confused-indian.html',
     ]
 
     def parse(self, response):
